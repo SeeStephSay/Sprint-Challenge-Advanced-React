7a28c121eb1b9020e1cf8696a68d5f36f2e90aa5
Stephanie Butenhof <1divinedevotion@gmail.com>
2020-01-27 23:28:13 -0600

    Added dark mode toggle to NavBar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { toggleDarkMode } from '../hooks/toggleDarkMode';
 
 const NavBar = () => {
+	const [ darkMode, setDarkMode ] = toggleDarkMode();
+
 	return (
 		<nav className='navbar'>
 			<h1>Women's World Cup Player Rankings</h1>
 			<h4>Based on worldwide Google searches dated June-July 2019</h4>
-			<Link to='/'>View Listings</Link>
+			<Link to='/'>View Rankings</Link>
+
+			<div className='dark-mode__toggle'>
+				<div onClick={setDarkMode} className={darkMode ? 'toggle toggled' : 'toggle'} />
+			</div>
 		</nav>
 	);
 };
