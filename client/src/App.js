6816cb8d9bf0f927bac306400d19839d1ace1715
Stephanie Butenhof <1divinedevotion@gmail.com>
2020-03-22 22:43:16 -0500

    Laid out App

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			players : [],
+		};
+	}
+
+	fetchPlayers = () => {
+		fetch('http://localhost:5000/api/players')
+			.then((response) => {
+				return response.json();
+			})
+			.then((players) => this.setState({ players: players }))
+			.catch((error) => {
+				console.log('Server not responding', error);
+			});
+	};
+
+	componentDidMount() {
+		this.fetchPlayers();
+	}
+
+	render() {
+		return (
+			<div className='App'>
+				<NavBar />
+				<div className='cardsContainer'>
+					{this.state.players.map((player) => (
+						<PlayerCard key={player.id} name={player.name} country={player.country} />
+					))}
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
