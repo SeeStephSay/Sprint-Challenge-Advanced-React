cf7883aacc10d18772ef754e77637540663762de
Stephanie Butenhof <1divinedevotion@gmail.com>
2020-03-23 00:22:21 -0500

    Added tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM, { render } from 'react-dom';
 import App from './App';
+import PlayerCard from './components/PlayerCard';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it('Renders without crashing', () => {
+	render(<App />);
+});
+
+test('Toggle loads dark mode', () => {
+	const { getByTestId } = render(<App />);
+	getByTestId('toggle-element');
+});
+
+test('Players are defined', () => {
+	const players = <PlayerCard />;
+	expect(players).toBeDefined();
+	expect(players).not.toBeUndefined();
+	expect(players).not.toBeFalsy();
+});
+
+test('Home page title works', () => {
+	const { getByText } = render(<App />);
+	getByText(/Women's World Cup Players/i);
 });
